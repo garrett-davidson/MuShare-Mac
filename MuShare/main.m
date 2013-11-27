5c3d2ab09950a858a65017634b980571e2fbc894
Garrett Davidson <davidson.garrettm@gmail.com>
2013-11-27 15:35:22 +0300

    Initial Commit

---
+++ MuShare/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MuShare
+//
+//  Created by Garrett Davidson on 11/27/13.
+//  Copyright (c) 2013 Garrett Davidson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
